0e0d133154c56e643bdbafd38c10ef98d2537e81
jinleileiking <jinleileiking@gmail.com>
2012-06-02 14:05:42 +0800

    roll ok

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# 简介
+
+* 系统启动后，将自动在聊天栏建立一个名为'掉落'的聊天栏，紫色以及以上的物品掉落和roll点数将在这个聊天栏显示
+
+# 捐赠
+
+* 您的支持是我坚持搞插件的动力，请给于些捐赠吧：http://ershou.taobao.com/item.htm?id=14923473002
+
+# 作者
+
+* 伊利丹-童童宝宝
+* 联系方式:jinleileiking.github.com
+* 插件主页:https://github.com/jinleileiking/lkLootChatMsg
+
--- lkLootChatMsg.lua
+++ lkLootChatMsg.lua
@@ -1,23 +1,102 @@
+MyAddon = LibStub("AceAddon-3.0"):NewAddon("Myaddon", "AceEvent-3.0")
 
-MyAddon = LibStub("AceAddon-3.0"):NewAddon("MyAddon", "AceEvent-3.0")
+g_chatframename = "掉落"
 
-MyAddon:RegisterEvent("SpecialEvents_ItemLooted", "LootMsg")
+function MyAddon:OnEnable()
+    print("|cffffff00紫色掉落物品插件已经加载(作者:童童宝宝-伊利丹)，将会在'" .. g_chatframename .. "'聊天栏里显示紫色及以上物品掉落和roll点数")
+    OpenNewWindow(g_chatframename)
+    MyAddon:RegisterEvent("CHAT_MSG_LOOT")
+    MyAddon:RegisterEvent("CHAT_MSG_SYSTEM")
+end
 
-function MyAddon:LootMsg(who, item, number)
-    FindFrame("Loot", who .. "ʰȡ��" .. item .. "x" .. number)
+function MyAddon:CHAT_MSG_SYSTEM(event, Text)
+    if (string.find(Text,"掷出")) then
+         FindFrame(g_chatframename, "|cffffff00" .. Text)
+        return
+    end
+end
+
+function MyAddon:CHAT_MSG_LOOT(event, Text)
+    if (string.find(Text,"|cff9d9d9d")) then
+        return
+    elseif (string.find(Text,"|cffffffff")) then
+        return
+    elseif (string.find(Text,"|cff1eff00")) then
+        --FindFrame("Loot", "A Green item" .. Text)
+        return		
+    end
+    FindFrame(g_chatframename, Text)
 end
 
 
---[[ Window and Chat Functions ]]--
 function FindFrame(toFrame, msg)
-	for i=1,NUM_CHAT_WINDOWS do
-		local name = GetChatWindowInfo(i)
-		if (toFrame == name) then
-			toFrame = _G["ChatFrame" .. i]
-			toFrame:AddMessage(msg)
-		elseif (toFrame ~= name) then
-		    --Todo
+    for i=1,NUM_CHAT_WINDOWS do
+        local name = GetChatWindowInfo(i)
+        if (toFrame == name) then
+            toFrame = _G["ChatFrame" .. i]
+            toFrame:AddMessage(msg)
+        elseif (toFrame ~= name) then
+            --Todo
         end
+    end
+end
+
+function OpenNewWindow(name)
+	local count = 1;
+	local chatFrame, chatTab;
+
+	for i=1, NUM_CHAT_WINDOWS do
+    local window_name = GetChatWindowInfo(i) 
+    ----print("--------------")
+    ----print(window_name)
+    ----print(name)
+
+    if window_name and window_name == name then
+      --print("close")
+      chatFrame = _G["ChatFrame"..i];
+      FCF_Close(chatFrame)
+    end
+    
+    --print("in")
+    local _, _, _, _, _, _, shown = FCF_GetChatWindowInfo(i);
+    chatFrame = _G["ChatFrame"..i];
+    chatTab = _G["ChatFrame"..i.."Tab"];
+    if ( (not shown and not chatFrame.isDocked) or (count == NUM_CHAT_WINDOWS) ) then
+      if ( not name or name == "" ) then
+        name = format(CHAT_NAME_TEMPLATE, i);			
+      end
+
+      --print("creating")
+      -- initialize the frame
+      FCF_SetWindowName(chatFrame, name);
+      FCF_SetWindowColor(chatFrame, DEFAULT_CHATFRAME_COLOR.r, DEFAULT_CHATFRAME_COLOR.g, DEFAULT_CHATFRAME_COLOR.b);
+      FCF_SetWindowAlpha(chatFrame, DEFAULT_CHATFRAME_ALPHA);
+      SetChatWindowLocked(i, nil);
+
+      -- clear stale messages
+      chatFrame:Clear();
+
+      -- Listen to the standard messages
+      ChatFrame_RemoveAllMessageGroups(chatFrame);
+      ChatFrame_RemoveAllChannels(chatFrame);
+      ChatFrame_ReceiveAllPrivateMessages(chatFrame);
+      ChatFrame_ReceiveAllBNConversations(chatFrame);
+
+      --Clear the edit box history.
+      chatFrame.editBox:ClearHistory();
+
+      -- Show the frame and tab
+      chatFrame:Show();
+      chatTab:Show();
+      SetChatWindowShown(i, 1);
+
+      -- Dock the frame by default
+      FCF_DockFrame(chatFrame, (#FCFDock_GetChatFrames(GENERAL_CHAT_DOCK)+1), true);
+      FCF_FadeInChatFrame(FCFDock_GetSelectedWindow(GENERAL_CHAT_DOCK));
+      --print("created")
+      return chatFrame;
+    end
+    count = count + 1;
 	end
 end
-	
+
